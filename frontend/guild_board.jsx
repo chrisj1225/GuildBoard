c0dc021d4b1998f5d1017a48cf17aa69b5b876d9
Chris Joo <chrisj1225@gmail.com>
2021-05-10 12:01:05 -0400

    Finished frontend skeleton/setup, installed npm packages, created entry file

---
+++ frontend/guild_board.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DomContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Welcome to GuildBoard</h1>, root)
+})
