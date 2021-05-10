43a4130e9f652f4081d1d4920d817f764ebd1fb1
Chris Joo <chrisj1225@gmail.com>
2021-05-10 16:43:25 -0400

    Created App and root, imported Provider & HashRouter, and updated entry

--- frontend/guild_board.jsx
+++ frontend/guild_board.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
-import { signup, login, logout } from './actions/session_actions';
+// import { signup, login, logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
   
-  //test functions:
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
-  window.store = store;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // test functions:
+  // window.signup = signup;
+  // window.login = login;
+  // window.logout = logout;
+  // window.store = store;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to GuildBoard</h1>, root)
+  ReactDOM.render(<Root store={store} />, root)
 })
