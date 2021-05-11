bc5e57c111e1617ecf8e96d4f932fd749f159cba
Chris Joo <chrisj1225@gmail.com>
2021-05-11 10:52:27 -0400

    Created server action creators

---
+++ frontend/actions/server_actions.js
@@ -0,0 +1,64 @@
+import * as ServerAPIUtil from '../util/server_api_util';
+
+export const RECEIVE_SERVERS = 'RECEIVE_SERVERS';
+export const RECEIVE_SERVER = 'RECEIVE_SERVER';
+export const REMOVE_SERVER = 'REMOVE_SERVER';
+export const RECEIVE_SERVER_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveServers = servers => {
+  return({
+    type: RECEIVE_SERVERS,
+    servers
+  })
+};
+
+export const receiveServer = server => {
+  return ({
+    type: RECEIVE_SERVER,
+    server
+  })
+};
+
+export const removeServer = serverId => {
+  return ({
+    type: REMOVE_SERVER,
+    serverId
+  })
+};
+
+export const receiveErrors = (errors) => {
+  return ({
+    type: RECEIVE_SERVER_ERRORS,
+    errors
+  })
+};
+
+export const fetchServers = () => dispatch => {
+  return ServerAPIUtil.fetchServers()
+    .then(servers => dispatch(receiveServers(servers)),
+    err => dispatch(receiveErrors(err.responseJSON)))
+};
+
+export const fetchServer = (serverId) => dispatch => {
+  return ServerAPIUtil.fetchServer(serverId)
+    .then(server => dispatch(receiveServer(server)),
+    err => dispatch(receiveErrors(err.responseJSON)))
+};
+
+export const createServer = (server) => dispatch => {
+  return ServerAPIUtil.createServer(server)
+    .then(server => dispatch(receiveServer(server)),
+    err => dispatch(receiveErrors(err.responseJSON)))
+};
+
+export const updateServer = (server) => dispatch => {
+  return ServerAPIUtil.updateServer(server)
+    .then(server => dispatch(receiveServer(server)),
+    err => dispatch(receiveErrors(err.responseJSON)))
+};
+
+export const deleteServer = (serverId) => dispatch => {
+  return ServerAPIUtil.deleteServer(serverId)
+    .then(() => dispatch(removeServer(serverId)),
+    err => dispatch(receiveErrors(err.responseJSON)))
+};
--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_ERRORS";
 
 // Regular Action Creators
 export const receiveCurrentUser = (currentUser) => {
@@ -21,7 +21,7 @@ export const logoutCurrentUser = () => {
 
 export const receiveErrors = (errors) => {
   return ({
-    type: RECEIVE_ERRORS,
+    type: RECEIVE_SESSION_ERRORS,
     errors
   })
 };
