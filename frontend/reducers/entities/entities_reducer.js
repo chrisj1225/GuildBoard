1bfa98772dda396e9b829d02ff1c48c5e8b1cdca
Chris Joo <chrisj1225@gmail.com>
2021-05-12 11:31:58 -0400

    Created serversReducer & serverErrorsReducer

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
+import serversReducer from './servers_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
