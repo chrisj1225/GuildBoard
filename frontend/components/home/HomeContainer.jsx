67c19b206d8218bd2e0f3871c8eef1a81e8c1f7c
Chris Joo <chrisj1225@gmail.com>
2021-05-15 00:33:59 -0400

    Progress on figuring out component hierarchy/architecture

--- frontend/components/home/HomeContainer.jsx
+++ frontend/components/home/HomeContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ServerSideBarContainer from '../server/server_sidebar/ServerSidebar_container';
 import ServerChannelListContainer from '../server/server_channel_list/ServerChannelList_container';
+// import ServerPanel from '../server/server_panel/ServerPanel';
 import Channel from '../channel/channel/Channel';
 
 const HomeContainer = () => (
