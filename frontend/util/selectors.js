dddc80a971c8757abe9990c608ae84a6036481b9
Chris Joo <chrisj1225@gmail.com>
2021-05-20 01:28:49 -0400

    Modified convertDateString selector

--- frontend/util/selectors.js
+++ frontend/util/selectors.js
@@ -38,13 +38,43 @@ export const getChannelMessages = (state, channelId) => {
 }
 
 export const convertDateString = (date) => {
-  let splitDate = date.split("-");
-  splitDate[2] = splitDate[2].slice(0,2); // ["2021", "05", "19"]
-  let numSplitDate = splitDate.map(n => (parseInt(n))); // [2021, 5, 19]
-  let newDate = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`; // "05/19/2021"
-  const currDate = new Date();
-  return newDate;
+  const msgDate = new Date(date);
+  const msgYear = msgDate.getFullYear();
+  const msgMonth = (msgDate.getMonth() + 1);
+  const msgDay = msgDate.getDate();
+  const newDate = `${msgMonth}/${msgDay}/${msgYear}`; // "05/19/2021"
+  
+  let hr = msgDate.getHours();
+  let min = msgDate.getMinutes();
+  let AMPM = 'AM';
 
-  // return newTimeStamp 
-  "2021-05-19T14:46:18.695Z"
+  if (hr > 12) {
+    hr -= 12;
+    AMPM = 'PM'
+  } else if (hr == 0) {
+    hr += 12;
+  }
+  if (min < 10) {
+    min = "0" + min
+  }
+  const msgTime = `${hr}:${min} ${AMPM}`
+
+  const currDate = new Date();
+  const currYear = currDate.getFullYear();
+  const currMonth = (currDate.getMonth() + 1);
+  const currDay = currDate.getDate();
+
+  if ((currYear === msgYear) && 
+    (currMonth === msgMonth) && 
+    (currDay === msgDay)){
+    return `Today at ${msgTime}`
+  }
+
+  if ((currYear === msgYear) && 
+    (currMonth === msgMonth) && 
+    (currDay === msgDay+1)){
+    return `Yesterday at ${msgTime}`
+  }
+
+  return newDate;
 }
